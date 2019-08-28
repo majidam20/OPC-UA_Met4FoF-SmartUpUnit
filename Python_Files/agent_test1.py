ba7fce366dd973ee4d391223559d1b98e0d1d9bb
majid aminian <majidam66@gmail.com>
2019-08-28 17:39:10 +0200

    Add files via upload

---
+++ Python_Files/agent_test1.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+#os.getcwd()
+
+
+# In[7]:
+
+
+#os.chdir("C:\\Users\\aminia01\\Desktop\\IOT\\agentMET4FOF-master\\agentMET4FOF-master")
+
+
+# In[1]:
+
+
+import google.protobuf as pf
+import sys
+import time
+import socket
+import messages_pb2
+import threading
+import pandas as pd
+import csv
+from datetime import datetime
+
+
+
+#from skmultiflow.data.file_stream import FileStream
+
+from agentMET4FOF.agents import AgentMET4FOF, AgentNetwork, MonitorAgent
+from agentMET4FOF.streams import SensorGenerator
+ 
+#Here we define a new agent SineGeneratorAgent, and override the functions : init_parameters & agent_loop
+#init_parameters() is used to setup the data stream and necessary parameters
+#agent_loop() is an infinite loop, and will read from the stream continuously,
+#then it sends the data to its output channel via send_output
+#Each agent has internal current_state which can be used as a switch by the AgentNetwork
+
+class SensorGeneratorAgent(AgentMET4FOF):
+
+    def init_parameters(self):
+            self.stream = SensorGenerator()
+            self.stream.prepare_for_use()
+            self.counter = 0
+            self.start = datetime.now().second
+
+    def agent_loop(self):
+        if self.current_state == "Running":
+            #sensor_data1,
+            sensor_data2 = self.stream.next_sample() #dictionary
+            self.counter +=1
+            print('starttttttttttttttttttttttttttttttttttttttttttttttt:   ' + str(self.start))
+            #self.log_info("Counter "+str(self.counter))
+            if (datetime.now().second)-self.start>=5:
+                print('enddddddddddddddddddddddddddddddddddddddddddddddddd:   ' + str(datetime.now().second))
+                print('outputttttttttttttttttttttttttttttttttttttttttttttt:   '+str(self.counter))
+            return self.send_output({'gps_data':sensor_data2})
+
+
+
+def main():
+    #start agent network server
+    agentNetwork = AgentNetwork()
+
+    #init agents by adding into the agent network
+    gen_agent = agentNetwork.add_agent(agentType= SensorGeneratorAgent)
+    gen_agent.init_agent_loop(loop_wait=.001)
+    monitor_agent = agentNetwork.add_agent(agentType= MonitorAgent)
+
+    #connect agents by either way:
+    # 1) by agent network.bind_agents(source,target)
+    agentNetwork.bind_agents(gen_agent, monitor_agent)
+
+    # 2) by the agent.bind_output()
+    gen_agent.bind_output(monitor_agent)
+
+    # set all agents states to "Running"
+    agentNetwork.set_running_state()
+
+    # allow for shutting down the network after execution
+    return agentNetwork
+
+
+if __name__ == '__main__':
+   main()
+
+if __name__ == '__main__':
+   agt_net = AgentNetwork(connect=True)
+   monitor_agent = agt_net.get_agent("MonitorAgent_1")
+   memory = monitor_agent.get_attr("memory")  # your collected sensor data should be here
+   print(memory)
+# In[ ]:
+
+
+
+
